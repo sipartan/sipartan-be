803f4f381f1c733eaebf9cd218bd404c1a684cef
Rozan Maulana <rozanmln.wp@gmail.com>
2023-10-11 00:19:03 +0700

    feat first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log('Server listening on ' + PORT);
+});
