02f2a634d4243ed1d63ab40c33e1abf4fe51c6f5
Rozan <rozanmln.wp@gmail.com>
2024-02-20 00:22:31 +0700

    save data

--- src/config/database.js
+++ src/config/database.js
@@ -6,19 +6,19 @@ require("dotenv").config();
 //   dialect: "postgres",
 // });
 
-const db = new Sequelize("sipartan-db", "postgres", "sipartan", {
-  host: "34.101.174.205",
-  dialect: "postgres",
-});
+// const db = new Sequelize("sipartan-db", "postgres", "sipartan", {
+//   host: "34.101.174.205",
+//   dialect: "postgres",
+// });
 
-// const db = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//   }
-// );
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  }
+);
 
 module.exports = db;
