0b3755d910943b7470388349a3275de1cd388858
Rozan Maulana <rozanmln.wp@gmail.com>
2024-05-13 11:05:49 +0700

    change db

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 // KALO MAU PUSH GIT GANTI DB NYA DULS
 
 // ===== WINDOWS =====
-const db = new Sequelize("sipartan_db", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-});
+// const db = new Sequelize("sipartan_db", "postgres", "postgres", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
 
 // ===== LINUX =====
 // const db = new Sequelize("sipartan-db-local", "postgres", "postgres", {
@@ -21,15 +21,15 @@ const db = new Sequelize("sipartan_db", "postgres", "postgres", {
 //   dialect: "postgres",
 // });
 
-// const db = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//     port: process.env.DB_PORT
-//   }
-// );
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT
+  }
+);
 
 module.exports = db;
